07b2400f6da8373bbf159937f3cdecff9eb66e58
sebasgross <sebasgrossmann@gmail.com>
2020-11-12 18:42:18 -0500

    Thank you response

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as toastr from 'toastr';
+import Router from 'next/router'
 import MailerServices from "../services/MailerServices";
 
 
@@ -21,6 +22,7 @@ class Contact extends Component {
             name: "",
             message: "",
         },
+        done: false,
  
         height: 0,
         sent: false,
@@ -44,42 +46,48 @@ class Contact extends Component {
        MailerServices.sendMail(data)
         .then((res) => {
             toastr.success("Message Sent")
-            this.setState(initialState)
+            this.setState({done:true})
         })
         .catch((error) => console.log(error))
     }
     
     render(){
 
-        const {height} = this.state;
+        const {height, done} = this.state;
+        console.log(this.state)
 
-        console.log(this.state);
-        return(
+        if (done === true) {
+                Router.push('/thank_you');
+        }else {
 
-            <div className="contact-page" height={height}>
-                <h1> Contact us - warm response guaranteed!</h1>
-                <p id="contact-us-message">If you have any questions or stories you'd like to share fill in the form below. We would also love to hear your feedback and any suggestions for future guests or topics.</p>
-                <p>You can also send an email over to <b>contact@reboundwithus.com</b></p>
-                <div className="contact-section">
-                    <div className="contact-inputs">
-                        <div>
-                            <input className="inputs-login-styling" placeholder="Name" name="name" onChange={this.onChange}></input>
-                            <input className="inputs-login-styling" placeholder="Email" name="email" onChange={this.onChange}></input>
+            return(
+
+                <div className="contact-page" height={height}>
+                    <h1> Contact us - warm response guaranteed!</h1>
+                    <p id="contact-us-message">If you have any questions or stories you'd like to share fill in the form below. We would also love to hear your feedback and any suggestions for future guests or topics.</p>
+                    <p>You can also send an email over to <b>contact@reboundwithus.com</b></p>
+                    <div className="contact-section">
+                        <div className="contact-inputs">
+                            <div>
+                                <input className="inputs-login-styling" placeholder="Name" name="name" onChange={this.onChange}></input>
+                                <input className="inputs-login-styling" placeholder="Email" name="email" onChange={this.onChange}></input>
+                            </div>
+                            <textarea className="textarea-styling" rows="4" name="message" onChange={this.onChange} placeholder="Your message..."></textarea>
+                            <div className="contact-button">
+                            <button className="submit-button" onClick={this.submit}>Submit</button>
+                            </div>
+                            <div>
+                            </div>
                         </div>
-                        <textarea className="textarea-styling" rows="4" name="message" onChange={this.onChange} placeholder="Your message..."></textarea>
-                        <div className="contact-button">
-                        <button className="submit-button" onClick={this.submit}>Submit</button>
-                        </div>
-                        <div>
+                        <div className="contact-address">
+                        <p><b>Miami, Fl, 33133</b></p>
                         </div>
                     </div>
-                    <div className="contact-address">
-                    <p><b>Miami, Fl, 33133</b></p>
-                    </div>
+    
                 </div>
-
-            </div>
-        )
+            )
+        }
+        
     }
 }
 
---
+++ pages/thank_you.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class ThankYou extends Component {
+    render () {
+        return (
+            <div className="thank-you-page">
+                <div className="thank-you-div">
+                    <h1>Thank You </h1>
+                    <p>Take it easy, breath in, breath out, we will respond shortly! </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ThankYou
