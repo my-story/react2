614a04d19fd2d4b2c6bd1b72b38906e58cd526c4
sebasgross <sebasgrossmann@gmail.com>
2020-11-18 17:46:54 -0500

    Clean up code

--- components/podcast/PodcastCard.js
+++ components/podcast/PodcastCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
-import PodcastServices from '../../services/PodcastServices';
+// import PodcastServices from '../../services/PodcastServices';
 
 class PodcastCard extends Component {
 
